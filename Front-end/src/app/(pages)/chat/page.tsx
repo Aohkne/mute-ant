4cbe159100c73bde847ce11ab29ab83c436cb5af
KHOA <aohkne@gmail.com>
2025-02-28 15:59:13 +0700

    done ui history chat

--- Front-end/src/app/(pages)/chat/page.tsx
+++ Front-end/src/app/(pages)/chat/page.tsx
@@ -4,9 +4,11 @@ import { Send, Trash } from "lucide-react";
 import Markdown from "react-markdown";
 import classNames from "classnames/bind";
 import styles from "./Chat.module.scss";
-import Header from "@/components/Header/Header";
 import Image from "next/image";
 import { useChatSession } from "./useChatSession";
+
+import Header from "@/components/Header/Header";
+import NavChat from "@/components/NavChat/NavChat";
 
 const cx = classNames.bind(styles);
 
@@ -19,10 +21,10 @@ const Chatbot: React.FC = () => {
   const [input, setInput] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const [history, setHistory] = useState<MessageItem[]>([
-
     {
       role: "model",
-      parts: "Rất hân hạnh được gặp bạn. Mình là mute-ant, chatbot của bạn. Bạn có muốn hỏi gì về khiếm thính, ngôn ngữ kí hiệu không?",
+      parts:
+        "Rất hân hạnh được gặp bạn. Mình là mute-ant, chatbot của bạn. Bạn có muốn hỏi gì về khiếm thính, ngôn ngữ kí hiệu không?",
     },
   ]);
   const messageContainerRef = useRef<HTMLDivElement | null>(null);
@@ -30,7 +32,8 @@ const Chatbot: React.FC = () => {
 
   useEffect(() => {
     if (messageContainerRef.current) {
-      messageContainerRef.current.scrollTop = messageContainerRef.current.scrollHeight;
+      messageContainerRef.current.scrollTop =
+        messageContainerRef.current.scrollHeight;
     }
   }, [history]);
 
@@ -97,101 +100,112 @@ const Chatbot: React.FC = () => {
     setHistory([
       {
         role: "model",
-        parts: "Rất hân hạnh được gặp bạn. Mình là mute-ant, chatbot của bạn. Bạn có muốn hỏi gì về khiếm thính, ngôn ngữ kí hiệu không?",
+        parts:
+          "Rất hân hạnh được gặp bạn. Mình là mute-ant, chatbot của bạn. Bạn có muốn hỏi gì về khiếm thính, ngôn ngữ kí hiệu không?",
       },
     ]);
     setInput("");
     resetChat();
   }
 
+  function handleSelectHistory(index: number): void {
+    alert(`Bạn đã chọn lịch sử chat ${index + 1}`);
+  }
+
   return (
     <div className={cx("wrapper")}>
-      <div className={cx("px-10")}>
-        <Header />
-      </div>
+      <NavChat onSelectHistory={handleSelectHistory} />
 
-      <main className={cx("content", "py-10")}>
-        <div className={cx("chat-container")}>
-          <div className={cx("message-container")} ref={messageContainerRef}>
-            {history.map((item, index) => (
-              <div
-                key={index}
-                className={cx(
-                  "chat-message",
-                  {
-                    "message-start": item.role === "model",
-                    "message-end": item.role !== "model",
-                  },
-                  {
-                    user: item.role !== "model",
-                  }
-                )}
-              >
-                <div className={cx("avatar-container")}>
+      <div className={cx("chat")}>
+        <div className={cx("px-10")}>
+          <Header />
+        </div>
+
+        <main className={cx("content")}>
+          <div className={cx("chat-container")}>
+            <div className={cx("message-container")} ref={messageContainerRef}>
+              {history.map((item, index) => (
+                <div
+                  key={index}
+                  className={cx(
+                    "chat-message",
+                    {
+                      "message-start": item.role === "model",
+                      "message-end": item.role !== "model",
+                    },
+                    {
+                      user: item.role !== "model",
+                    }
+                  )}
+                >
+                  <div className={cx("avatar-container")}>
+                    <div
+                      className={cx("avatar-wrapper", {
+                        user: item.role !== "model",
+                      })}
+                    >
+                      <Image
+                        alt={item.role === "model" ? "Gemini" : "User"}
+                        src={
+                          item.role === "model"
+                            ? "/images/ant.png"
+                            : "/images/author/LHK.png"
+                        }
+                        width={50}
+                        height={50}
+                        className={cx("avatar-image")}
+                      />
+                    </div>
+                  </div>
+
                   <div
-                    className={cx("avatar-wrapper", {
+                    className={cx("message-bubble", {
                       user: item.role !== "model",
                     })}
                   >
-                    <Image
-                      alt={item.role === "model" ? "Gemini" : "User"}
-                      src={
-                        item.role === "model"
-                          ? "/images/ant.png"
-                          : "/images/author/LHK.png"
-                      }
-                      width={50}
-                      height={50}
-                      className={cx("avatar-image")}
-                    />
+                    <div
+                      className={cx(
+                        item.role !== "model"
+                          ? "chat-text-user"
+                          : "chat-text-bot"
+                      )}
+                    >
+                      <Markdown>{item.parts}</Markdown>
+                    </div>
                   </div>
                 </div>
+              ))}
+            </div>
 
-                <div
-                  className={cx("message-bubble", {
-                    user: item.role !== "model",
-                  })}
-                >
-                  <div
-                    className={cx(
-                      item.role !== "model" ? "chat-text-user" : "chat-text-bot"
-                    )}
-                  >
-                    <Markdown>{item.parts}</Markdown>
-                  </div>
-                </div>
-              </div>
-            ))}
+            <div className={cx("input-container")}>
+              <button className={cx("reset-button")} onClick={reset}>
+                <Trash className={cx("icon-small")} />
+              </button>
+              <textarea
+                value={input}
+                rows={1}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={handleKeyDown}
+                placeholder="Start Chatting..."
+                className={cx("chat-input", "text-title")}
+              />
+              <button
+                className={cx("send-button", {
+                  loading: loading,
+                })}
+                onClick={chatting}
+                disabled={loading}
+              >
+                {loading ? (
+                  <span className={cx("spinner")} />
+                ) : (
+                  <Send className={cx("icon-small")} />
+                )}
+              </button>
+            </div>
           </div>
-
-          <div className={cx("input-container")}>
-            <button className={cx("reset-button")} onClick={reset}>
-              <Trash className={cx("icon-small")} />
-            </button>
-            <textarea
-              value={input}
-              rows={1}
-              onChange={(e) => setInput(e.target.value)}
-              onKeyDown={handleKeyDown}
-              placeholder="Start Chatting..."
-              className={cx("chat-input", "text-title")}
-            />
-            <button
-              className={cx("send-button", {
-                loading: loading,
-              })}
-              onClick={chatting}
-              disabled={loading}
-            >
-              {loading ? (
-                <span className={cx("spinner")} />
-              ) : (
-                <Send className={cx("icon-small")} />
-              )}
-            </button>
-          </div>
-        </div>
-      </main>
+        </main>
+      </div>
     </div>
   );
 };
