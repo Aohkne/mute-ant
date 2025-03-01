98f8392ca52b69620a876efa7a43795617545025
LTP <lamphat279@gmail.com>
2025-03-01 17:44:18 +0700

    update

--- Back-end/mute-ant/src/main/java/muteant/muteant/model/dto/response/AuthResponse.java
+++ Back-end/mute-ant/src/main/java/muteant/muteant/model/dto/response/AuthResponse.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Data
 @Builder
 public class AuthResponse {
+    private Long userId;
     private String accessToken;
     private String refreshToken;
     private String role;
